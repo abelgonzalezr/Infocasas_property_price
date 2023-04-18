25dfbf3819021b5d088c87dcf79f47dff7358e05
Abel Gonzalez <abelgr45@gmail.com>
2023-04-17 23:50:41 -0400

    add comment and create a method

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
+// Import the Axios library
 const axios = require('axios');
 
+// Set the API URL
 const INFOCASAS_API = 'https://graph.infocasas.com.uy/graphql'
 
-const propertyId = process.argv[2]; // Get the property ID from the command line argument
+// Get the property ID from the command line argument
+const propertyId = process.argv[2];
 
-const query = `
-  query {
-    property(id: ${propertyId}) {
-      price {
-        amount
-        currency {
-          name
+// Define an asynchronous function to get the property price
+async function getPropertyPrice(Id) {
+
+  // Set the GraphQL query to retrieve the property price
+  const query = `
+    query {
+      property(id: ${Id}) {
+        price {
+          amount
+          currency {
+            name
+          }
         }
       }
     }
-  }
-`;
+  `;
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'X-Origin': 'www.infocasas.com.uy'
-};
+  // Set the HTTP request headers
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'X-Origin': 'www.infocasas.com.uy'
+  };
 
-axios.post(INFOCASAS_API, { query }, { headers })
-  .then(response => {
-    if(response.data.data.property === null) return console.log(`price not found for id ${propertyId}`)
+  // Make the HTTP request using Axios
+  axios.post(INFOCASAS_API, { query }, { headers })
+    .then(response => {
+      // If the property is not found, return an error message
+      if(response.data.data.property === null) return console.log(`price not found for id ${Id}`)
 
-    const data = response.data.data.property.price;
+      // If the property is found, return the price in the corresponding currency
+      const data = response.data.data.property.price;
+      return console.log(`${data.currency.name} ${data.amount} `);
+    })
+    .catch(error => console.error(error));
+}
 
-    console.log(`${data.currency.name} ${data.amount} `);
-  })
-  .catch(error => console.error(error));
+// Call the function to get the property price
+getPropertyPrice(propertyId);
